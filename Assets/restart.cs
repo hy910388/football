8dad52e459670e5c7a099e8b2f8db11b2a743a91
CindyLiang <127974575+hy910388@users.noreply.github.com>
2025-04-01 18:08:10 +0800

    0401

---
+++ Assets/restart.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void Startgame()
+    {
+        //¸ü¤J³õ´º
+        SceneManager.LoadScene(0);
+    }
+}
